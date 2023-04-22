84eb8ac46839472b1a07b49741c664ce389bfbb5
dag25 <dag25@mail.ru>
2023-04-22 21:09:14 +0400

    API_URL

--- src/modules/const.js
+++ src/modules/const.js
@@ -1,15 +1,15 @@
 export const COUNT_PAGINATION = 3;
 export const DATA = {};
-export const API_URL = 'https://everlasting-placid-chime.glitch.me';
+export const API_URL = 'https://available-unmarred-eagle.glitch.me';
 export const TITLE = {
-  women: {
-    title: 'Новая коллекция Бюстгальтер-балконет ',
-    id: '2269903069'
-  },
-  men: {
-    title: 'Боксеры из новой коллекции',
-    id: '4413664778'
-  },
+	women: {
+		title: 'Новая коллекция Бюстгальтер-балконет ',
+		id: '2269903069',
+	},
+	men: {
+		title: 'Боксеры из новой коллекции',
+		id: '4413664778',
+	},
 };
 
 export const header = document.querySelector('.header');
